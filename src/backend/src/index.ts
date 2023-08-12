091af89275b85edd6e208759b56e30d8361b4546
Vitor L <vitoribeiro0024@gmail.com>
2023-08-12 11:46:40 -0300

    Adição de Documentação

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -3,22 +3,24 @@ import chalk from "chalk";
 import { UniRentDataSource } from "./config/UniRentDataSource.js";
 import { routes, routesSecurity } from "./routes.js";
 import { UsuarioController } from "./controller/UsuarioController.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json" assert { type: 'json' }
 import cors from 'cors';
-UniRentDataSource.initialize().then(async (c)=>{
-const app = express();
+UniRentDataSource.initialize().then(async (c) => {
+    const app = express();
 
-app.use(
-    express.json(),
-    cors(),
-    routes,
-    routesSecurity
-);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+    app.use(
+        express.json(),
+        cors(),
+        routes,
+        routesSecurity
+    );
 
-
-app.listen(3000, ()=> console.log(chalk.blue(`Servidor escutando a porta 3000`)));
+    app.listen(3000, () => console.log(chalk.blue(`Servidor escutando a porta 3000`)));
 
 }
-).catch((error)=>{console.log(chalk.red(`Erro na inicialização da conexão com o banco de dados: ${error}`))})
+).catch((error) => { console.log(chalk.red(`Erro na inicialização da conexão com o banco de dados: ${error}`)) })
 
 
 
